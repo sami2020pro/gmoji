87a8c76c44d20e1450bb2133fb0ffeea98c55235
sami2020pro <64816951+sami2020pro@users.noreply.github.com>
2020-11-09 03:17:39 +0330

    Update fontawesome.go

--- fontawesome.go
+++ fontawesome.go
@@ -9,7 +9,7 @@ package gmoji
 **/
 
 const (
-	all                     Gmoji = All
+	all                     Gmoji = Gmoji
 	Glass                   Gmoji = "\uf000"
 	Music                   Gmoji = "\uf001"
 	Search                  Gmoji = "\uf002"
